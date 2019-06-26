66502f7033e3022b5e5a99a1338e01d1d5faadf7
Matt Leuer <mattleuer@gmail.com>
2019-06-26 16:14:56 -0700

    implemented calculateCarpoolDiscount method.

--- src/main/java/TollBooth.java
+++ src/main/java/TollBooth.java
@@ -5,6 +5,25 @@ public class TollBooth {
     public BigDecimal calculateToll(Vehicle vehicle) {
         BigDecimal toll = new BigDecimal(0.50);
 
+        toll = calculateAxleUpCharge(vehicle, toll);
+        toll = calculateCarpoolDiscount(vehicle, toll);
+        return toll;
+    }
+
+    private BigDecimal calculateCarpoolDiscount(Vehicle vehicle, BigDecimal toll) {
+
+        if (vehicle.occupants > 2) {
+
+            BigDecimal perPassengerDiscount = new BigDecimal(0.25);
+            BigDecimal extraPassengerDiscountMultiplier = new BigDecimal(vehicle.occupants - 2);
+            BigDecimal passengerDiscount = perPassengerDiscount.multiply(extraPassengerDiscountMultiplier);
+            BigDecimal discountedToll = toll.subtract(passengerDiscount);
+            toll = discountedToll;
+        }
+        return toll;
+    }
+
+    private BigDecimal calculateAxleUpCharge(Vehicle vehicle, BigDecimal toll) {
         if (vehicle.axles > 2) {
             BigDecimal extraAxles = new BigDecimal(vehicle.axles - 2.0);
             BigDecimal perAxleSurcharge = new BigDecimal(0.50);
@@ -14,4 +33,6 @@ public class TollBooth {
         }
         return toll;
     }
+
+
 }
--- src/test/java/TollBoothTest.java
+++ src/test/java/TollBoothTest.java
@@ -28,4 +28,14 @@ public class TollBoothTest {
            Assert.assertEquals(0, actualToll.compareTo(expectedToll));
     }
 
+    @Test
+    public void calculateTollShouldDiscountTwentyFiveCentsForPassengersMoreThanTwo() {
+            Vehicle vehicle = new Vehicle("Ecto-1", "Cadillac", "Fleetwood", Year.of(2000), 2, 3);
+            TollBooth tollBooth = new TollBooth();
+
+            BigDecimal actualToll = tollBooth.calculateToll(vehicle);
+            BigDecimal expectedToll = new BigDecimal(0.25);
+
+            Assert.assertEquals(0, actualToll.compareTo(expectedToll));
+    }
 }
